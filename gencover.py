e0817b46487faa389cd5f3c36b20c35c56c846eb
Farid Saleh <github.farid1@gmail.com>
2023-07-25 10:42:40 +0900

    update scripts

--- gencover.py
+++ gencover.py
@@ -1,68 +1,14 @@
 #ok
 import json
-from sqlalchemy import create_engine, Column, Integer, String, SmallInteger, Text, CHAR, select
-from sqlalchemy.orm import sessionmaker, DeclarativeBase,  relationship
-
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped, Session
-from sqlalchemy.orm import mapped_column
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import os
 import fitz
 from os.path import exists
+from settings import *
+from dbclass import Doc, Department, Bundle
 
-
-DEPARTMENT_TABLE = "department"
-BUNDLE_TABLE = "bundle"
-DOC_TABLE = "doc"
-TABLE_PREFIX = "alihmedia_inactive_"
-DATABASE = "arsipdb3"
-COVER_LOCATION = os.path.join("/home/farid/dev/python/Site-Django-Authentication-System", "static", "alihmedia_inactive", "cover")
-PDF_LOCATION = "/home/farid/pdfs/"
-
-
-
-engine = create_engine('mysql+pymysql://root:1234@localhost:33061/arsipdb3', echo=False)
-
-class Base(DeclarativeBase):
-    pass
-
-class Department(Base):
-    __tablename__ =  TABLE_PREFIX + DEPARTMENT_TABLE
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(100))
-    defcode: Mapped[str] = mapped_column(String(20))
-    link: Mapped[str] = mapped_column(String(20))
-
-class Bundle(Base):
-    __tablename__ = TABLE_PREFIX + BUNDLE_TABLE
-    id: Mapped[int] = mapped_column(primary_key=True)
-    department_id: Mapped[int] = mapped_column(ForeignKey(TABLE_PREFIX + DEPARTMENT_TABLE + ".id"))
-    box_number: Mapped[int] = mapped_column(SmallInteger)
-    bundle_number: Mapped[int] = mapped_column(SmallInteger)
-    code: Mapped[str] = mapped_column(String(20))
-    title: Mapped[str] = mapped_column(Text)
-    year: Mapped[str] = mapped_column(CHAR(4),  nullable=True)
-    orinot: Mapped[str] = mapped_column(String(10), nullable=True)
-    description: Mapped[str] = mapped_column(String(255), nullable=True)
-    department = relationship("Department")
-
-
-class Doc(Base):
-    __tablename__ = TABLE_PREFIX + DOC_TABLE
-    id: Mapped[int] = mapped_column(primary_key=True)
-    bundle_id: Mapped[int] = mapped_column(ForeignKey(TABLE_PREFIX + BUNDLE_TABLE + ".id"))
-    doc_number: Mapped[int] = mapped_column(SmallInteger)
-    doc_count: Mapped[int] = mapped_column(SmallInteger)
-    orinot: Mapped[str] = mapped_column(String(10), nullable=True)
-    doc_type: Mapped[str] = mapped_column(String(20), nullable=True)
-    description: Mapped[str] = mapped_column(Text, nullable=True)
-    page_count: Mapped[str] = mapped_column(SmallInteger, nullable=True)
-    filesize: Mapped[int] = mapped_column(SmallInteger, nullable=True)
-    bundle = relationship("Bundle")
-    # comments = relationship("Comment")
-
+engine = create_engine('mysql+pymysql://{}:{}@localhost:{}/{}'.format(USER, PASSWORD, PORT, DBNAME) , echo=False)
 
 def get_size(file_path, unit='bytes'):
     file_size = os.path.getsize(file_path)
@@ -95,7 +41,7 @@ def main():
     for row in result:
         path = os.path.join(PDF_LOCATION, row.bundle.department.link, str(row.bundle.box_number), str(row.doc_number) + ".pdf")
         if exists(path):
-            coverfilename = "{}_{}_{}.png".format(row.bundle.department.link, row.bundle.box_number, row.doc_number)
+            coverfilename = "{}{}_{}_{}.png".format(TABLE_PREFIX, row.bundle.department.link, row.bundle.box_number, row.doc_number)
             generatecover(pdffile=path, coverfilename=coverfilename)
             filesize = get_size(path, "kb")
             pagecount = get_page_count(pdffile=path)
---
+++ settings.py
@@ -0,0 +1,11 @@
+import os
+DBNAME = "db4"
+PORT = "33061"
+USER = "root"
+PASSWORD = "1234"
+DEPARTMENT_TABLE = "department"
+BUNDLE_TABLE = "bundle"
+DOC_TABLE = "doc"
+TABLE_PREFIX = "alihmedia_inactive_"
+COVER_LOCATION = os.path.join("/home/farid/dev/python/bwsmalut2/", "static", "cover")
+PDF_LOCATION = "/home/farid/pdfs/"
