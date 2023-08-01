a49cb53a8a943a2c922c8e2e03b2d1eb444cd2ee
Farid Saleh <github.farid1@gmail.com>
2023-08-01 13:51:44 +0900

    update scripts

--- startngrok.sh
+++ startngrok.sh
@@ -9,7 +9,7 @@ while ! nc -z localhost 4040; do
 done
 
 # Get NGROK dynamic URL from its own exposed local API
-NGROK_REMOTE_URL="$(curl http://127.0.0.1:4040/api/tunnels | jq ".tunnels[0].public_url")"
+NGROK_REMOTE_URL="$(curl http://localhost:4040/api/tunnels | jq ".tunnels[0].public_url")"
 if test -z "${NGROK_REMOTE_URL}"
 then
   echo "❌ ERROR: ngrok doesn't seem to return a valid URL (${NGROK_REMOTE_URL})."
@@ -37,5 +37,5 @@ PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
 
 # bold=$(tput bold)
 # normal=$(tput sgr0)
-echo ${NGROK_REMOTE_URL} | tr -d '\n' | pbcopy
+echo ${NGROK_REMOTE_URL} | tr -d '\n'
 printf "\n\n🌍 Your ngrok remote URL is 👉 ${bold}${NGROK_REMOTE_URL} 👈\n📋 ${normal}I've just copied it to your clipboard 😉\n\n"
